11ac24ac65279707bde6c2277a962182db5a136d
Akshaya H <81792372+my-my414@users.noreply.github.com>
2021-07-08 08:54:21 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,14 @@
+//take a sides of triangles the triangle has three sides
+
+t=6
+m=6
+h=7
+
+//calculate the semi perimeter
+
+s=(t+m+h)/2
+
+//calculate the area of  the triangle
+
+area=(s*(s-a)*(s-b)*(s-c))**0.5
+print("The area of the triangle is",area)
